7871c074c39142bb6f7002965abcd74ef86adead
Vladimir Kudinov <frux@yandex-team.ru>
2015-12-17 12:10:22 +0500

    First scetch

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-/**
- * Created by frux on 17/12/15.
- */
+var stream = require('stream');
+
+function StreamTransformer(destStream, transformFunc){
+	stream.Transform.call(this);
+	this._transformFunc = transformFunc;
+	this.pipe(destStream);
+}
+
+StreamTransformer.prototype = new stream.Transform();
+
+StreamTransformer.prototype._transform = function(chunk, enc, cb){
+	cb(null, this._transformFunc(chunk));
+};
+
+StreamTransformer.wrap = function(sourceStream, transformFunction){
+	return new StreamTransformer(sourceStream, transformFunction);
+};
+
+module.exports = StreamTransformer;
